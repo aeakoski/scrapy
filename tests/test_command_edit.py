162c2f3898489e3e19a9b1c5631785488d30174e
Oskar Ek <oskarek24@gmail.com>
2018-02-28 11:45:01 +0100

    add accidentally uncommited test

---
+++ tests/test_command_edit.py
@@ -0,0 +1,12 @@
+from twisted.trial import unittest
+
+from scrapy.utils.testsite import SiteTest
+from scrapy.utils.testproc import ProcessTest
+
+from scrapy.commands.edit import Command
+
+class FetchTest(ProcessTest, SiteTest, unittest.TestCase):
+
+    def test_edit_command_syntax(self):
+        command = Command()
+        self.assertEqual(command.syntax(), "<spider>")
