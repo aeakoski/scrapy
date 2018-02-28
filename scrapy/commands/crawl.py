624a5c8b9d94ddee636498ff8b97a8fdc644d432
Oskar Ek <oskarek24@gmail.com>
2018-02-28 10:41:07 +0100

    remove accidental change

--- scrapy/commands/crawl.py
+++ scrapy/commands/crawl.py
@@ -4,6 +4,7 @@ from scrapy.utils.conf import arglist_to_dict
 from scrapy.utils.python import without_none_values
 from scrapy.exceptions import UsageError
 
+
 class Command(ScrapyCommand):
 
     requires_project = True
